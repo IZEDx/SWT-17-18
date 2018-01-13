cf51218805039497e71acfcf628a4b3f62f9ca7d
Niklas Kühtmann <admin@i-zed.net>
2018-01-13 17:39:00 +0100

    Basic Webserver hinzugefügt, wenn er gestartet ist kann man darüber auf Dateien aus html/ zugreifen.

--- Prototyp/src/main.ts
+++ Prototyp/src/main.ts
@@ -1,4 +1,16 @@
 
+import express = require("express");
+import { static as serveStatic } from "express";
+import { join } from "path";
+
+const path = (...str: string[]) => join(__dirname, "..", ...str);
+
 export async function main(args: string[]) {
-    console.log("Hello World");
+    const port = parseInt(args[2], 10);
+    const app = express();
+
+    app.use(serveStatic(path("html")));
+
+    console.log(`Starting WebServer on port ${port}`)
+    app.listen(port);
 }
