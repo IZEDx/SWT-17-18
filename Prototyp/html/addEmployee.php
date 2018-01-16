f5b8bdb351392ac55756f98a54a07530e01008d4
R0qs3T <torbenrogge@gmail.com>
2018-01-16 01:18:57 +0100

    Trasnlated to German.

--- Prototyp/html/addEmployee.php
+++ Prototyp/html/addEmployee.php
@@ -76,13 +76,13 @@
         <div class="table-title">
             <div class="row">
                 <div class="col-sm-6">
-                    <h2>Manage <b>Employees</b></h2>
+                    <h2>Mitarbeiter <b>Verwalten</b></h2>
                 </div>
                 <div class="col-sm-6">
                     <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i>
-                        <span>Add New Employee</span></a>
+                        <span>Mitarbeiter Hinzufügen</span></a>
                     <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i>
-                        <span>Delete</span></a>
+                        <span>Löschen</span></a>
                 </div>
             </div>
         </div>
@@ -97,9 +97,9 @@
                 </th>
                 <th>Name</th>
                 <th>Email</th>
-                <th>Address</th>
-                <th>Phone</th>
-                <th>Actions</th>
+                <th>Addresse</th>
+                <th>Telefonnummer</th>
+                <th>Aktionen</th>
             </tr>
             </thead>
             <tbody>
