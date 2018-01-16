e73b7609cd0724ebfe176de6e3eaff2998aa4f91
R0qs3T <torbenrogge@gmail.com>
2018-01-16 18:33:33 +0100

    Changed php code, now casting json-data to array.

--- Prototyp/html/addEmployee.php
+++ Prototyp/html/addEmployee.php
@@ -2,47 +2,47 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-            <meta name="description" content="">
-            <meta name="author" content="">
-            <link rel="icon" href="img/favicon.ico">
+    <title>Mitarbeiter Verwalten</title><!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="img/favicon.ico">
 
-            <title>Mitarbeiter Hinzufügen</title>
+        <title>Mitarbeiter Hinzufügen</title>
 
-            <!-- Bootstrap core CSS -->
-            <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
-            <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-            <link href="bootstrap/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="bootstrap/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
-            <!-- Custom styles for this template -->
-            <link href="styles/employeeList.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="styles/employeeList.css" rel="stylesheet">
 
-            <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-            <!--[if lt IE 9]>
-            <script src="bootstrap/docs/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-            <script src="bootstrap/docs/assets/js/ie-emulation-modes-warning.js"></script>
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]>
+        <script src="bootstrap/docs/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <script src="bootstrap/docs/assets/js/ie-emulation-modes-warning.js"></script>
 
-            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-            <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            <![endif]-->
-        </head>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
 
 
 <body>
@@ -224,23 +224,15 @@
     </div>
 </div>
 
+
+
+
 <div style="width: 100%;height: 250px; background-color: #00CC00">
     <p><?php
         $jsonFile = file_get_contents("test.json");
         //$jsonFile = callAPI("GET", "api/employees", $jsonFile);
-        echo json_decode($jsonFile);
-        $jsonIterator = new RecursiveIteratorIterator(
-            new RecursiveArrayIterator(json_decode($jsonFile, TRUE)),
-            RecursiveIteratorIterator::SELF_FIRST);
-        foreach ($jsonIterator
-
-                 as $key => $val) {
-            if (is_array($val)) {
-                echo "$key:\n";
-            } else {
-                echo "$key => $val\n";
-            }
-        }
+        $arr = json_decode($jsonFile, true);
+        var_dump($arr);
         ?></p>
 </div>
 
